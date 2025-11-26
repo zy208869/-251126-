9da3d9202b9ae9f4268406b4375ed1532b485a28
zy208869 <3175469250@qq.com>
2025-11-26 23:32:34 +0800

    更新 MyMod.cpp

--- src/mod/MyMod.cpp
+++ src/mod/MyMod.cpp
@@ -1,7 +1,18 @@
 #include "mod/MyMod.h"
-
 #include "ll/api/mod/RegisterHelper.h"
-
+#include "MyPMod.h"
+#include "Config.h"
+#include <ll/api/Config.h>
+#include <ll/api/command/CommandHandle.h>
+#include <ll/api/command/CommandRegistrar.h>
+#include <ll/api/data/KeyValueDB.h>
+#include <ll/api/event/EventBus.h>
+#include <ll/api/event/player/PlayerJoinEvent.h>
+#include <ll/api/event/player/PlayerUseItemEvent.h>
+#include <ll/api/service/Bedrock.h>
+#include <mc/server/commands/CommandOrigin.h>
+#include <mc/server/commands/CommandOutput.h>
+#include <mc/world/actor/player/Player.h>
 namespace my_mod {
 
 MyMod& MyMod::getInstance() {
@@ -26,12 +37,11 @@ bool MyMod::disable() {
     // Code for disabling the mod goes here.
     return true;
 }
-
-bool MyMod::unload(){
+bool MyMod::unload() {
     getSelf().getLogger().debug("unloading...");
+    // Code for disabling the mod goes here.
     return true;
 }
-
 } // namespace my_mod
 
 LL_REGISTER_MOD(my_mod::MyMod, my_mod::MyMod::getInstance());
